a2b3f661d8006e2356906b5213c7a24deb59fa05
Marco Robutti <marcorobutti69@gmail.com>
2026-03-17 09:07:12 +0100

    aggiunta init_db per cloud

--- app.py
+++ app.py
@@ -4,6 +4,52 @@ import sqlite3
 
 def get_connection():
     return sqlite3.connect("hotel_ai.db")
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # USERS TABLE
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT,
+        password TEXT,
+        hotel TEXT
+    )
+    """)
+
+    # BOOKINGS TABLE
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS bookings (
+        date TEXT,
+        rooms_sold INTEGER,
+        hotel TEXT
+    )
+    """)
+
+    # REVENUES TABLE
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS revenues (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT,
+        department TEXT,
+        revenue REAL,
+        hotel TEXT
+    )
+    """)
+
+    # USER DEMO
+    cursor.execute("SELECT * FROM users")
+    if not cursor.fetchall():
+        cursor.execute("""
+        INSERT INTO users (username, password, hotel)
+        VALUES ('admin', 'admin123', 'Hotel Demo')
+        """)
+
+    conn.commit()
+    conn.close()
+
+# inizializza database
+init_db()
 
 st.set_page_config(layout="wide")
 
